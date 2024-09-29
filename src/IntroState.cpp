d01a6d99ce2159602bca0455d95e57e6fb43bc20
metcalfinl <finlaymetcalfe@hotmail.com>
2024-09-29 16:14:24 +1300

    Implemented third cutscene, allowed for up down left right panning of slides, start game after cutscene completes

--- src/IntroState.cpp
+++ src/IntroState.cpp
@@ -1,6 +1,6 @@
 #include "IntroState.hpp"
-#include <SDL2/SDL_image.h>
 #include <MissionState.hpp>
+#include <SDL2/SDL_image.h>
 #include <algorithm>
 
 /**
@@ -12,30 +12,40 @@ IntroState::IntroState(App &app) : app(app) {
     // Clear Renderables
     app.ClearRenderables();
 
-    SDL_Surface* slideImage0 = IMG_Load("./assets/images/CutScene1.png");
+    SDL_Surface *slideImage0 = IMG_Load("./assets/images/CutScene1.png");
 
-    m_slideshow = std::make_shared<TexturedRect2D>(-1.4/2, -1.2/2, 1.2, 1.2);
+    m_slideshow = std::make_shared<TexturedRect2D>(SLIDE_RIGHT, SLIDE_MIDDLE,
+                                                   SLIDE_ZOOM, SLIDE_ZOOM);
     m_slideshow->UpdateVertexData();
     m_slideshow->SetTextureFormat(GL_RGB);
-    m_slideshow->SetTexture(slideImage0->pixels, slideImage0->w, slideImage0->h);
+    m_slideshow->SetTexture(slideImage0->pixels, slideImage0->w,
+                            slideImage0->h);
     m_slides[0].slideSurface = slideImage0;
-    m_slides[0].startX = -1.4/2;
-    m_slides[0].stopX = -0.5;
-    m_slides[0].startY = -1.2/2;
-    m_slides[0].stopY = -1.2/2;
-    m_slides[0].panSpeedMS = 5000.0;
+    m_slides[0].startX = SLIDE_RIGHT;
+    m_slides[0].stopX = SLIDE_LEFT;
+    m_slides[0].startY = SLIDE_MIDDLE;
+    m_slides[0].stopY = SLIDE_MIDDLE;
+    m_slides[0].panSpeedMS = 4 * SECOND_MS;
 
     app.AddRenderable(m_slideshow);
 
-    SDL_Surface* slideImage1 = IMG_Load("./assets/images/CutScene2.png");
+    SDL_Surface *slideImage1 = IMG_Load("./assets/images/CutScene2.png");
 
     m_slides[1].slideSurface = slideImage1;
-    m_slides[1].startX = -0.5;
-    m_slides[1].stopX = -0.5;
-    m_slides[1].startY = -1.4/2;
-    m_slides[1].stopY = -0.5;
-    m_slides[1].panSpeedMS = 5000.0;
+    m_slides[1].startX = SLIDE_MIDDLE;
+    m_slides[1].stopX = SLIDE_MIDDLE;
+    m_slides[1].startY = SLIDE_RIGHT;
+    m_slides[1].stopY = SLIDE_LEFT;
+    m_slides[1].panSpeedMS = 3 * SECOND_MS;
 
+    SDL_Surface *slideImage2 = IMG_Load("./assets/images/CutScene3.png");
+
+    m_slides[2].slideSurface = slideImage2;
+    m_slides[2].startX = SLIDE_LEFT;
+    m_slides[2].stopX = SLIDE_RIGHT;
+    m_slides[2].startY = SLIDE_BOTTOM;
+    m_slides[2].stopY = SLIDE_BOTTOM;
+    m_slides[2].panSpeedMS = 4 * SECOND_MS;
 }
 
 /**
@@ -50,7 +60,8 @@ void IntroState::Run() {
     // Render Sequence
     app.Render();
 
-    if (m_currSlide == SLIDE_COUNT){
+    if (m_currSlide == SLIDE_COUNT) {
+        app.SetState(new MissionState(app));
         return;
     }
     Slide slide = m_slides[m_currSlide];
@@ -63,25 +74,29 @@ void IntroState::Run() {
     }
 
     float panXLength = slide.stopX - slide.startX;
+    float nextXStep = currX + (panXLength / slide.panSpeedMS / app.DeltaTime());
+    float nextX = slide.stopX > slide.startX ? std::min(nextXStep, slide.stopX)
+                                             : std::max(nextXStep, slide.stopX);
+
     float panYLength = slide.stopY - slide.startY;
-    float nextX = panXLength == 0 ? slide.stopY : std::min((float) (currX + (panXLength / slide.panSpeedMS / app.DeltaTime())), slide.stopX);
-    float nextY = panYLength == 0 ? slide.stopY : std::min((float) (currY + (panYLength / slide.panSpeedMS / app.DeltaTime())), slide.stopY);
-    m_slideshow->SetPosition(
-            nextX,
-            nextY);
+    float nextYStep = currY + (panYLength / slide.panSpeedMS / app.DeltaTime());
+    float nextY = slide.stopY > slide.startY ? std::min(nextYStep, slide.stopY)
+                                             : std::max(nextYStep, slide.stopY);
+    m_slideshow->SetPosition(nextX, nextY);
     m_slideshow->UpdateGL();
 }
 
 void IntroState::NextSlide() {
     m_currSlide++;
 
-    if (m_currSlide >= SLIDE_COUNT){
+    if (m_currSlide >= SLIDE_COUNT) {
         return;
     }
 
     Slide slide = m_slides[m_currSlide];
-    SDL_Surface* slideSurface = slide.slideSurface;
-    m_slideshow->SetTexture(slideSurface->pixels, slideSurface->w, slideSurface->h);
+    SDL_Surface *slideSurface = slide.slideSurface;
+    m_slideshow->SetTexture(slideSurface->pixels, slideSurface->w,
+                            slideSurface->h);
     m_slideshow->SetPosition(slide.startX, slide.startY);
     m_slideshow->UpdateGL();
 }
